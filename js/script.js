84262d172d67707edc4e0afe54cd0a3692a62ba4
Orsu Anil <orsuorsuanil272@gmail.com>
2026-07-05 21:22:33 +0530

    Update Railway domain

--- js/script.js
+++ js/script.js
@@ -94,7 +94,7 @@ async function loadRestaurants() {
 
     try {
 
-        let response = await fetch("https://foodie-backend-production-4b6f.up.railway.app/restaurants");
+       let response = await fetch("https://foodie-backend-production-81b4.up.railway.app/restaurants");
 
         let restaurants = await response.json();
         console.log(restaurants);
